41423800a7d95795c7172e6ca3f6989dcbe6d129
Sasa Nedic <svetasmak0@gmail.com>
2024-01-16 00:45:01 +0100

    Create ToDO class

--- Todo-List/script.js
+++ Todo-List/script.js
@@ -2,3 +2,13 @@ const input = document.querySelector(".taskInput");
 const btnAdd = document.querySelector(".addBtn");
 const displayElement = document.querySelector(".taskList");
 const removeElement = document.querySelector(".remove");
+console.log(removeElement);
+
+class ToDO {
+  constructor(text) {
+    this.text = text;
+  }
+  addToDo() {
+    return `<div class="newEl"><li>${this.text}</li> <span class="remove">❌</span> </div>`;
+  }
+}
