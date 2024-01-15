5058ad493494303f636eeebd8f2ebeb94cbf7689
Sasa Nedic <svetasmak0@gmail.com>
2024-01-16 00:44:34 +0100

    Select all variables

---
+++ Todo-List/script.js
@@ -0,0 +1,4 @@
+const input = document.querySelector(".taskInput");
+const btnAdd = document.querySelector(".addBtn");
+const displayElement = document.querySelector(".taskList");
+const removeElement = document.querySelector(".remove");
