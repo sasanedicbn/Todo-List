0639cf32e501b7beb82129d42410e16459e230b6
Sasa Nedic <svetasmak0@gmail.com>
2024-01-17 15:17:32 +0100

    Change code

--- Todo-List/script.js
+++ Todo-List/script.js
@@ -1,39 +1,36 @@
 const input = document.querySelector(".taskInput");
 const btnAdd = document.querySelector(".addBtn");
 const displayElement = document.querySelector(".taskList");
-const removeElement = document.querySelector(".remove");
-console.log(removeElement);
 
 class ToDO {
-  constructor(text) {
+  constructor(text, id) {
     this.text = text;
+    this.id = Math.floor(Math.random() * 100000);
   }
+
   addToDo() {
-    return `<div class="newEl"><li>${this.text}</li> <span class="remove">❌</span> </div>`;
+    return `<div class="newEl" data-id="${this.id}"><li>${this.text}</li> <span class="remove">❌</span> </div>`;
   }
 }
+
 class ToDoManager {
-  constructor(todos) {
+  constructor() {
     this.todos = [];
   }
+
   addToDo(todo) {
     this.todos.push(todo);
   }
-  deleteToDo(id) {
-    // this.id.slice(id, 1);
-  }
 }
+
 const todoMangers = new ToDoManager();
+
 btnAdd.addEventListener("click", function () {
   let value = input.value.trim();
   if (value !== "") {
     let instanca1 = new ToDO(value);
-    console.log(instanca1.addToDo());
-
     todoMangers.addToDo(instanca1);
-
     displayElement.innerHTML += instanca1.addToDo();
-    console.log(instanca1);
   }
-  value = "";
+  input.value = "";
 });
